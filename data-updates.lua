1be7cab66a508471cead160bbcbd97b14d2518a1
LCruel <dvda_l@hotmail.com>
2016-05-05 20:16:00 +0200

    First release

---
+++ data-updates.lua
@@ -0,0 +1,3 @@
+data.raw["recipe"]["straight-rail"].result_count = data.raw["recipe"]["straight-rail"].result_count * 5
+data.raw["recipe"]["curved-rail"].result_count = data.raw["recipe"]["curved-rail"].result_count * 5
+data.raw["recipe"]["concrete"].result_count = data.raw["recipe"]["concrete"].result_count * 10 
